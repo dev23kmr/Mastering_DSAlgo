900e4fa99ac22d6fd1225565bc08b90f53567959
dev23kmr <dey23kmr@gmail.com>
2020-05-15 21:21:14 +0530

    text file

--- src/org/programing/datastructure/AddTwoLinkedList.java
+++ src/org/programing/datastructure/AddTwoLinkedList.java
@@ -11,10 +11,8 @@ class ListNode {
 
 public class AddTwoLinkedList {
 
-
-
-	class Solution {
-
+	//Solution 1:
+	
 		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
 			ListNode   temp = l1;
@@ -30,46 +28,157 @@ public class AddTwoLinkedList {
 					if(head==null){
 						head = new ListNode(sum, null);
 						curr = head;
-						carry = 0;
 					}else{
 						curr.next = new ListNode(sum,null);
 						curr = curr.next;
 					}
+					carry=0;
 				}else{
 					int r = sum % 10;
-					curr.next = new ListNode(r, null);
-					curr = curr.next;
-					carry = 1;                   
-				}
-				temp =  temp.next;
-				temp = temp2.next;
-			}
-
-			while(temp!=null){
-				sum = temp.val + carry;
-				if(sum < 10){
 					if(head==null){
-						head = new ListNode(sum, null);
+						head = new ListNode(r, null);
 						curr = head;
-						carry = 0;
+					}else{			
+					curr.next = new ListNode(r, null);
+					carry = 1;                   
+				 }
+				}
+				curr = curr.next;
+				temp =  temp.next;
+				temp2 = temp2.next;
+			}
+			
+			if(temp==null&&temp2==null){
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
+   
+			while(temp!=null){
+				sum = temp.val + carry; 
+				if(sum < 10){
+					curr.next = new ListNode(sum,null);
+					curr  = curr.next;
+					carry = 0;
 					}else{
-						curr.next = new ListNode(sum,null);
-						curr = curr.next;
-					}
-				}else{
 					int r = sum % 10;
 					curr.next = new ListNode(r, null);
 					curr = curr.next;
 					carry = 1;                   
 				}
-
-			}
 
 			temp =  temp.next;  
+			if(temp==null)
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
 			
-			return temp;
+			while(temp2!=null){
+				sum = temp2.val + carry; 
+				if(sum < 10){
+					curr.next = new ListNode(sum,null);
+					curr  = curr.next;
+					carry = 0;
+					}else{
+					int r = sum % 10;
+					curr.next = new ListNode(r, null);
+					curr = curr.next;
+					carry = 1;                   
+				}
+
+			temp2 =  temp2.next; 
+			if(temp2==null)
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
+					
+			return head;
 		}
-	}
+	//Solution 2:
+		
+		public ListNode addTwoNumbers_2(ListNode l1, ListNode l2) {
+
+			ListNode   temp = l1;
+			ListNode   temp2 = l2;
+			ListNode   head = null;
+			ListNode   curr = null;
+			int carry = 0;     
+			int sum = 0;
+
+			while(temp!=null && temp2!=null){
+				sum = temp.val + temp2.val + carry;
+				if(sum < 10){
+					temp.val = sum;
+					temp2.val = sum;
+					if(head==null){					
+						head = l1;					
+						carry = 0;
+					}
+				}else{
+					int r = sum % 10;
+					temp.val  = r;
+					temp2.val = r;
+					carry = 1;                   
+				}
+				
+				if(temp.next==null)
+					curr = temp;
+				
+				if(temp2.next==null)
+					curr =temp2;
+				
+				temp =  temp.next;
+				temp2 = temp2.next;
+			}
+			
+			if(temp==null&&temp2==null){
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
+   
+			while(temp!=null){
+				sum = temp.val + carry; 
+				if(sum < 10){
+					curr.next = new ListNode(sum,null);
+					curr  = curr.next;
+					carry = 0;
+					}else{
+					int r = sum % 10;
+					curr.next = new ListNode(r, null);
+					curr = curr.next;
+					carry = 1;                   
+				}
+
+			temp =  temp.next;  
+			if(temp==null)
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
+			
+			while(temp2!=null){
+				sum = temp2.val + carry; 
+				if(sum < 10){
+					curr.next = new ListNode(sum,null);
+					curr  = curr.next;
+					carry = 0;
+					}else{
+					int r = sum % 10;
+					curr.next = new ListNode(r, null);
+					curr = curr.next;
+					carry = 1;                   
+				}
+
+			temp2 =  temp2.next; 
+			if(temp2==null)
+				if(carry==1)
+					curr.next = new ListNode(1,null);
+			}
+					
+			return head;
+		}
+		
+		
+		
+		
 }
 
 
